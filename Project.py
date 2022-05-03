82f2efb40cd09986badbd8ef0e84332404b90a19
GabrielPena147 <gpena2@mail.sfsu.edu>
2022-05-02 19:16:38 -0700

    Added the Data folder that contains the data for DATA1A so only change data1B using data on your own machine

--- Project.py
+++ Project.py
@@ -1,24 +1,30 @@
 
 import SimpleITK as sitk
 
+'''Data1A is some random healthy lung used to compare the data in data1B'''
+'''Data1A should not be changed only Data1B'''
+data1A = "./Data/manifest-1608266677008/MIDRC-RICORD-1A/MIDRC-RICORD-1A-419639-000082/08-02-2002-NA-CT CHEST WITHOUT CONTRAST-04614/2.000000-ROUTINE CHEST NON-CON-97100"
 
-data1A = "C:/Users/gabri/Desktop/Data/manifest-1608266677008/MIDRC-RICORD-1A/MIDRC-RICORD-1A-419639-000082/08-02-2002-NA-CT CHEST WITHOUT CONTRAST-04614/2.000000-ROUTINE CHEST NON-CON-97100"
+'''Data1B can be a healthy or covid lung and is what we are testing'''
+
 '''data1B = "C:/Users/gabri/Desktop/Data/manifest-1608266677008/MIDRC-RICORD-1A\MIDRC-RICORD-1A-419639-000800/05-11-2005-NA-CT CHEST WITHOUT CONTRAST-61055/2.000000-ROUTINE CHEST NON-CON-70218"'''
-data1B = "C:/Users/gabri/Desktop/Data/manifest-1608266677008/MIDRC-RICORD-1A/MIDRC-RICORD-1A-419639-000361/10-21-2002-NA-CT CHEST WITHOUT CONTRAST-91670/2.000000-ROUTINE CHEST NON-CON-50599"
+'''data1B = "C:/Users/gabri/Desktop/Data/manifest-1608266677008/MIDRC-RICORD-1A/MIDRC-RICORD-1A-419639-000361/10-21-2002-NA-CT CHEST WITHOUT CONTRAST-91670/2.000000-ROUTINE CHEST NON-CON-50599"'''
 '''data1B = "C:/Users/gabri/Desktop/Data/manifest-1612365584013/MIDRC-RICORD-1B/MIDRC-RICORD-1B-419639-000350/02-09-2006-NA-CT CHEST WITHOUT CONTRAST-03488/2.000000-ROUTINE CHEST NON-CON-95169"'''
-'''data1B = "C:/Users/gabri/Desktop/Data/manifest-1612365584013/MIDRC-RICORD-1B/MIDRC-RICORD-1B-419639-000425/05-28-2002-NA-CT CHEST WITHOUT CONTRAST-88172/2.000000-ROUTINE CHEST NON-CON-50407"'''
+data1B = "C:/Users/gabri/Desktop/Data/manifest-1612365584013/MIDRC-RICORD-1B/MIDRC-RICORD-1B-419639-000425/05-28-2002-NA-CT CHEST WITHOUT CONTRAST-88172/2.000000-ROUTINE CHEST NON-CON-50407"
 
 def command_iteration(method):
     print(f"{method.GetOptimizerIteration():3} = {method.GetMetricValue():7.5f} : {method.GetOptimizerPosition()}")
 
+"""Transform based Registration"""
+'''Bspline too long for runtime'''
 def myReg3(source, target):
     pixelType = sitk.sitkFloat32
 
-    '''fixed = sitk.ReadImage(, sitk.sitkFloat32)'''
+    
     fixed = sitk.Normalize(source)
     fixed = sitk.DiscreteGaussian(fixed, 2.0)
 
-    '''moving = sitk.ReadImage(image, sitk.sitkFloat32)'''
+    
     moving = sitk.Normalize(target)
     moving = sitk.DiscreteGaussian(moving, 2.0)
 
@@ -75,12 +81,12 @@ image1B = reader2.Execute();
 feature_img = sitk.GradientMagnitude(image1A)
 feature_img2 = sitk.GradientMagnitude(image1B)
 
-
+'''Watershed segmentation'''
 '''Change the level value'''
 ws_img1 = sitk.MorphologicalWatershed(feature_img, level=50, markWatershedLine=True, fullyConnected=False)
-ws_img2 = sitk.MorphologicalWatershed(feature_img2, level=100, markWatershedLine=True, fullyConnected=False)
+ws_img2 = sitk.MorphologicalWatershed(feature_img2, level=50, markWatershedLine=True, fullyConnected=False)
 
-
+'''Registration function returns an image and the metric value'''
 regIMG, mVal = myReg3(image1A, ws_img2)
 
 
